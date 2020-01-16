30e05a043e12e1172558709a0230a6b16a3fb37f
usman2109 <40023326+usman2109@users.noreply.github.com>
2020-01-16 16:31:39 +0530

    timestamp commit

--- hello.java
+++ hello.java
@@ -1,10 +1,17 @@
-class hello{
- public static void main(String[] args) 
- { 
- int i=0;
-  for (i=0;i<5;i++)
-  {
- System.out.println("This line is %d",i); 
-  }
- }
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class TimeStampDemo 
+{
+    public static void main( String[] args )
+    {
+         //Date object
+	 Date date= new Date();
+         //getTime() returns current time in milliseconds
+	 long time = date.getTime();
+         //Passed the milliseconds to constructor of Timestamp class 
+	 Timestamp ts = new Timestamp(time);
+	 System.out.println("Current Time Stamp: "+ts);
+    }
 }
