00429cc0892e28710b3f6618ab45764c43360494
usman2109 <40023326+usman2109@users.noreply.github.com>
2020-01-16 16:34:08 +0530

    basic commit hello world

--- hello.java
+++ hello.java
@@ -1,17 +1,6 @@
-
-import java.sql.Timestamp;
-import java.util.Date;
-
-public class TimeStampDemo 
-{
-    public static void main( String[] args )
-    {
-         //Date object
-	 Date date= new Date();
-         //getTime() returns current time in milliseconds
-	 long time = date.getTime();
-         //Passed the milliseconds to constructor of Timestamp class 
-	 Timestamp ts = new Timestamp(time);
-	 System.out.println("Current Time Stamp: "+ts);
-    }
-}
+class hello { 
+ public static void main(String[] args) 
+ { 
+ System.out.println("Hello World!"); 
+ } 
+} 
